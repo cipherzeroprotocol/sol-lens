c8f5191626cb078a06e4d52eeb00dc2cb97e4bc6
Cipher Zero Protocol <cipherzeroprotocol@gmail.com>
2025-04-21 03:20:50 +0300

    Resolve merge conflicts

---
+++ solana-lens/fix-push.sh
@@ -0,0 +1,12 @@
+# First, fetch the remote changes
+git fetch origin
+
+# Option 1: If you want to keep your changes and merge with remote
+git pull origin main
+
+# Option 2: If you want to force your version (use with caution)
+# git push --force origin main
+
+# Option 3: If you want to rebase your changes on top of remote
+# git pull --rebase origin main
+# git push origin main
